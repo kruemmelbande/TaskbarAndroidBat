277ae4248204efe6852cf20c8cfa66ea10ff0122
kruemmelbande <kai.scheufen@gmail.com>
2022-10-01 13:46:48 +0200

    If the tmp dir is not found, the script will create it.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+iname="icon.ico"
+ipath="C:\\tmp\\"
+
+
 from infi.systray import SysTrayIcon
 from PIL import Image, ImageDraw,ImageFont
 import time
@@ -7,21 +11,23 @@ si = subprocess.STARTUPINFO()
 si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 def on_quit_callback(systray):
     os._exit(0)
-image= "C:\\tmp\\pil_text.ico"
+image= ipath+iname
+if not os.path.isdir(ipath):
+    os.mkdir(ipath)
 n=1
 bat=0
 rawbat=""
 last=-1
-try:    
+try:
     while True:
         # create image
-        img = Image.new('RGBA', (50, 50), color = (0, 0, 0, 0))  # color background =  white  with transparency
+        img = Image.new('RGBA', (50, 50), color = (0, 0, 0, 0))  # color1 background =  white  with transparency
         d = ImageDraw.Draw(img)
         #d.rectangle([(0, 40), (50, 50)], fill=(255, 255, 255), outline=None)  #  color = blue
         #add text to the image
         font_type  = ImageFont.truetype("arial.ttf", 50)
         try:
-            rawbat=subprocess.check_output(['adb', 'shell', 'dumpsys', 'battery'], startupinfo=si).decode("utf-8") 
+            rawbat=subprocess.check_output(['adb', 'shell', 'dumpsys', 'battery'], startupinfo=si).decode("utf-8")
         except:
             rawbat=""
         try:
@@ -47,13 +53,13 @@ try:
                 last=bat
                 bat="N/A"
                 if bat!=last:
-                    font_type  = ImageFont.truetype("arial.ttf", 30)   
-                    d.text((0,0), bat, fill=(255,0,0), font = font_type)            
+                    font_type  = ImageFont.truetype("arial.ttf", 30)
+                    d.text((0,0), bat, fill=(255,0,0), font = font_type)
                     img.save(image)
         except Exception as e:
             #print(e)
             time.sleep(10)
-        # display image in systray 
+        # display image in systray
         if n==1:
             systray = SysTrayIcon(image, "Android battery percentage",on_quit=on_quit_callback)
             systray.start()
